499141808f87ef95b07ba21ad3522221ddbce33d
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 15:03:03 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/admins/loop.py
+++ BrandrdXMusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ValenciaXMusic import app
-from ValenciaXMusic.utils.database import get_loop, set_loop
-from ValenciaXMusic.utils.decorators import AdminRightsCheck
-from ValenciaXMusic.utils.inline import close_markup
+from BrandrdXMusic import app
+from BrandrdXMusic.utils.database import get_loop, set_loop
+from BrandrdXMusic.utils.decorators import AdminRightsCheck
+from BrandrdXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
