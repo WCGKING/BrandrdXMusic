320a7959f7c7b246d349cb6c66b206d06d940b03
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 14:08:24 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/inline/song.py
+++ BrandrdXMusic/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_CHAT}",
+                text="⛦ sᴜᴩᴩᴏʀᴛ 🦋", url=f"{config.SUPPORT_CHAT}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
