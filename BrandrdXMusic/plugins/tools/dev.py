e6eb86ddca9960fb2d1338122d5f3a4ab2b25996
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 19:43:58 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/tools/dev.py
+++ BrandrdXMusic/plugins/tools/dev.py
@@ -10,7 +10,7 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from ValenciaXMusic import app
+from BrandrdXMusic import app
 from config import OWNER_ID
 
 
