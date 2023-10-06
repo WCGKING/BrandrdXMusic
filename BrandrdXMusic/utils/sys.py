a150b01bd805e8e3b4b3fb85890113cf617bf8f1
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 13:33:13 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/sys.py
+++ BrandrdXMusic/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from ValenciaXMusic.misc import _boot_
-from ValenciaXMusic.utils.formatters import get_readable_time
+from BrandrdXMusic.misc import _boot_
+from BrandrdXMusic.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
