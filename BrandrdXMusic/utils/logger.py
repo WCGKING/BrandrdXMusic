ad821ba4ff9b8dd0de45e5789303485347302e62
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 13:30:36 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/logger.py
+++ BrandrdXMusic/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from ValenciaXMusic import app
-from ValenciaXMusic.utils.database import is_on_off
+from BrandrdXMusic import app
+from BrandrdXMusic.utils.database import is_on_off
 from config import LOGGER_ID
 
 
