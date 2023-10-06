3abef93cc9c647beb157c5af5f03f4aef48dcefe
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 14:19:28 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/decorators/language.py
+++ BrandrdXMusic/utils/decorators/language.py
@@ -1,6 +1,6 @@
 from strings import get_string
-from ValenciaXMusic.misc import SUDOERS
-from ValenciaXMusic.utils.database import (get_lang, is_maintenance)
+from BrandrdXMusic.misc import SUDOERS
+from BrandrdXMusic.utils.database import (get_lang, is_maintenance)
 
 
 def language(mystic):
