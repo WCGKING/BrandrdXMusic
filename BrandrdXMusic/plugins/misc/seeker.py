0c72658d5f976cff87c6bd468576f0b1179eea01
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 18:29:36 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/misc/seeker.py
+++ BrandrdXMusic/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from ValenciaXMusic.misc import db
-from ValenciaXMusic.utils.database import get_active_chats, is_music_playing
+from BrandrdXMusic.misc import db
+from BrandrdXMusic.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
