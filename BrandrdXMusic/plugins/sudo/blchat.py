40be14b831b114ad732e2f65b3798d24069c69f7
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 19:02:18 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/sudo/blchat.py
+++ BrandrdXMusic/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ValenciaXMusic import app
-from ValenciaXMusic.misc import SUDOERS
-from ValenciaXMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from ValenciaXMusic.utils.decorators.language import language
+from BrandrdXMusic import app
+from BrandrdXMusic.misc import SUDOERS
+from BrandrdXMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from BrandrdXMusic.utils.decorators.language import language
 from config import BANNED_USERS
 
 
