489d60cd2587addc91332a97242c3167d0e9678c
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 20:11:14 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/tools/stats.py
+++ BrandrdXMusic/plugins/tools/stats.py
@@ -135,4 +135,3 @@ async def bot_stats(client, CallbackQuery, _):
         await CallbackQuery.message.reply_photo(
             photo=config.STATS_IMG_URL, caption=text, reply_markup=upl
         )
-
