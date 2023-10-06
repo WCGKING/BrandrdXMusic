5f14d17d0b2f9f0b3c74b41163801392ab23dcab
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 14:17:41 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/decorators/admins.py
+++ BrandrdXMusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from ValenciaXMusic import app
-from ValenciaXMusic.misc import SUDOERS, db
-from ValenciaXMusic.utils.database import (
+from BrandrdXMusic import app
+from BrandrdXMusic.misc import SUDOERS, db
+from BrandrdXMusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
