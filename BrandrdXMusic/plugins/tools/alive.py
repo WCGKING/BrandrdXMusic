454163e6568ac0513ea533c6f6a984fe589925ce
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 19:32:16 +0530

    Add files via upload

---
+++ BrandrdXMusic/plugins/tools/alive.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from ValenciaXMusic import app
+from pyrogram import filters
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from config import MUSIC_BOT_NAME
+
+@app.on_message(filters.command(["alive"]))
+async def start(client: Client, message: Message):
+    await message.reply_video(
+        video=f"https://te.legra.ph/file/7c52ce2b96657defd38fd.mp4",
+        caption=f"❤️ ʜᴇʏ {message.from_user.mention}\n\n🔮 ɪ ᴀᴍ {MUSIC_BOT_NAME}\n\n✨ ɪ ᴀᴍ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.\n\n💫 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ ᴊᴏɪɴ ᴏᴜʀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤍...\n\n━━━━━━━━━━━━━━━━━━❄",
+        reply_markup=InlineKeyboardMarkup(
+            [
+               [
+            InlineKeyboardButton(
+                text="🔮 ˹ᴘɪʏᴜsʜ˼", url=f"https://t.me/JoinIndianNavy_007"
+            ),
+            InlineKeyboardButton(
+                text="❤‍🔥 ᴍᴇᴇᴛ ᴍᴇ", url=f"https://t.me/Cherished_Community"
+            ),
+        ],
+                [
+            InlineKeyboardButton(
+                text="🇮🇳 ᴀʙᴏᴜᴛ ᴍᴇ", url=f"https://t.me/PiyushMalviyaOfficially"
+            ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "✯ ᴄʟᴏsᴇ ✯", callback_data="close"
+                    )
+                ],
+            ]
+        )
+    )
