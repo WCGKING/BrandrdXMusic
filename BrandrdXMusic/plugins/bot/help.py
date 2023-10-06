f11d9ebe2a41c3f80f9797022fced8723adf1669
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 15:20:00 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/bot/help.py
+++ BrandrdXMusic/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from ValenciaXMusic import app
-from ValenciaXMusic.utils import help_pannel
-from ValenciaXMusic.utils.database import get_lang
-from ValenciaXMusic.utils.decorators.language import LanguageStart, languageCB
-from ValenciaXMusic.utils.inline.help import help_back_markup, private_help_panel
+from BrandrdXMusic import app
+from BrandrdXMusic.utils import help_pannel
+from BrandrdXMusic.utils.database import get_lang
+from BrandrdXMusic.utils.decorators.language import LanguageStart, languageCB
+from BrandrdXMusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
