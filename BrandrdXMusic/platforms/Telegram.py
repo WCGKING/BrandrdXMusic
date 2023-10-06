951d4db946b93bfacd6c473b0057590765daad37
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 20:21:35 +0530

    BrandrdXMusic

--- BrandrdXMusic/platforms/Telegram.py
+++ BrandrdXMusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from ValenciaXMusic import app
-from ValenciaXMusic.utils.formatters import (
+from BrandrdXMusic import app
+from BrandrdXMusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
