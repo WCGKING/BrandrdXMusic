ca6b10168911107a77dc555bb418aaa0e9250063
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 20:23:25 +0530

    BrandrdXMusic

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ValenciaXMusic.utils.database import is_on_off
-from ValenciaXMusic.utils.formatters import time_to_seconds
+from BrandrdXMusic.utils.database import is_on_off
+from BrandrdXMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
