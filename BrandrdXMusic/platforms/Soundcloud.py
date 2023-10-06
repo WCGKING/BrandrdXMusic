5b2636fd34abf75e64ab9fecee261c25dbfa93a0
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 20:18:36 +0530

    BrandrdXMusic

--- BrandrdXMusic/platforms/Soundcloud.py
+++ BrandrdXMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from ValenciaXMusic.utils.formatters import seconds_to_min
+from BrandrdXMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
