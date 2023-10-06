53a6c17541d3aa390461bf7939383e2c51d1bd40
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-06 13:49:33 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/inline/help.py
+++ BrandrdXMusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from ValenciaXMusic import app
+from BrandrdXMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
