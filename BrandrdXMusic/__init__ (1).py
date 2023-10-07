cc001ca19a01e78dbac0a50049617b0947466567
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-07 15:18:24 +0530

    Add files via upload

---
+++ BrandrdXMusic/__init__ (1).py
@@ -0,0 +1,37 @@
+from VipX.core.bot import VipXBot
+from VipX.core.dir import dirr
+from VipX.core.git import git
+from VipX.core.userbot import Userbot
+from VipX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = VipXBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
