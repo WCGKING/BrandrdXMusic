af8d229e7c769b7fa1e40193d1a5ebb4d9d68681
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-07 15:36:17 +0530

    Update __main__.py

--- BrandrdXMusic/__main__.py
+++ BrandrdXMusic/__main__.py
@@ -21,7 +21,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        LOGGER(BrandrdXMusic).error("Assistant client variables not defined, exiting...")
         exit()
     await sudo()
     try:
