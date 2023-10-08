149c75bc9558d1c386e9a0d227b269ca958d15ea
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-08 10:59:12 +0530

    Update logging.py

--- BrandrdXMusic/logging.py
+++ BrandrdXMusic/logging.py
@@ -10,7 +10,7 @@ logging.basicConfig(
     ],
 )
 
-logging.getLogger("httpx").setLevel(logging.ERROR)
+
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
