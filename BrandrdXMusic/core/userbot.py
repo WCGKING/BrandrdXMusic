48b9ea30285f3c7070b57bd173a40b3a28479f85
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-08 10:37:00 +0530

    Update userbot.py

--- BrandrdXMusic/core/userbot.py
+++ BrandrdXMusic/core/userbot.py
@@ -46,7 +46,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
+                await self.one.join_chat("BRANDED_WORLD1")
                 await self.one.join_chat("BRANDRD_BOT")
             except:
                 pass
@@ -67,7 +67,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
+                await self.two.join_chat("BRANDED_WORLD1")
                 await self.one.join_chat("BRANDRD_BOT")
             except:
                 pass
@@ -88,7 +88,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
+                await self.three.join_chat("BRANDED_WORLD1")
                 await self.one.join_chat("BRANDRD_BOT")
             except:
                 pass
@@ -109,7 +109,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
+                await self.four.join_chat("BRANDED_WORLD1")
                 await self.one.join_chat("BRANDRD_BOT")
             except:
                 pass
@@ -130,7 +130,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BRANDED_WORLD")
+                await self.five.join_chat("BRANDED_WORLD1")
                 await self.one.join_chat("BRANDRD_BOT")
             except:
                 pass
