f320ab533916e23b919cc86c91c4b8d0d8639ce7
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-08 08:20:06 +0530

    Update logging.py

--- BrandrdXMusic/logging.py
+++ BrandrdXMusic/logging.py
@@ -1,15 +1,19 @@
 import logging
+from logging.handlers import RotatingFileHandler
+
+from config import LOG_FILE_NAME
 
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        logging.FileHandler("log.txt"),
+        RotatingFileHandler(
+            LOG_FILE_NAME, maxBytes=5000000, backupCount=10
+        ),
         logging.StreamHandler(),
     ],
 )
-
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
