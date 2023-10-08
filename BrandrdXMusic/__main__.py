46e90a3eec4208347d9836e267bafa6fd523f57a
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-08 12:09:15 +0530

    Update __main__.py

--- BrandrdXMusic/__main__.py
+++ BrandrdXMusic/__main__.py
@@ -1,18 +1,16 @@
 import asyncio
 import importlib
-import sys
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BANNED_USERS
 from BrandrdXMusic import LOGGER, app, userbot
 from BrandrdXMusic.core.call import Hotty
+from BrandrdXMusic.misc import sudo
 from BrandrdXMusic.plugins import ALL_MODULES
 from BrandrdXMusic.utils.database import get_banned_users, get_gbanned
-
-loop = asyncio.get_event_loop()
+from config import BANNED_USERS
 
 
 async def init():
@@ -23,17 +21,9 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("BrandrdXMusic").error(
-            "🙄 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 😐"
-        )
-        return
-    if (
-        not config.SPOTIFY_CLIENT_ID
-        and not config.SPOTIFY_CLIENT_SECRET
-    ):
-        LOGGER("BrandrdXMusic").warning(
-            "🥲 𝐒𝐢𝐫 𝐒𝐩𝐨𝐭𝐢𝐟𝐲 𝐈𝐝 & 𝐒𝐞𝐜𝐫𝐞𝐭 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝. 𝐃𝐨𝐧𝐭 𝐖𝐨𝐫𝐫𝐲 𝐍𝐨𝐭 𝐏𝐫𝐨𝐛𝐥𝐞𝐦 𝐄𝐧𝐣𝐨𝐲 𝐓𝐞𝐧𝐬𝐢𝐨𝐧 𝐅𝐫𝐞𝐞 🥰"
-        )
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -45,17 +35,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("BrandrdXMusic.plugins." + all_module)
-    LOGGER("BrandrdXMusic.plugins").info(
-        "😋 𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲 🎉"
-    )
+        importlib.import_module("BrandrdXMusic.plugins" + all_module)
+    LOGGER("BrandrdXMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await BrandrdXMusic.start()
-    await BrandrdXMusic.decorators()
-    LOGGER("BrandrdXMusic").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    await Hotty.start()
+    try:
+        await Hotty.stream_call("https://te.legra.ph/file/a0c2e8dae9f17911aef70.mp4")
+    except NoActiveGroupCall:
+        LOGGER("BrandrdXMusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Hotty.decorators()
+    LOGGER("BrandrdXMusic").info(
+        "\x56\x61\x6c\x65\x6e\x63\x69\x61\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x2e\x2e\x2e\x20\x0a\x0a\x44\x72\x6f\x70\x20\x59\x6f\x75\x72\x20\x47\x69\x72\x6c\x66\x72\x69\x65\x6e\x64\x27\x73\x20\x4e\x75\x6d\x62\x65\x72\x20\x41\x74\x20\x40\x4a\x6f\x69\x6e\x49\x6e\x64\x69\x61\x6e\x4e\x61\x76\x79\x5f\x30\x30\x37\x20\x0a\x0a\x4a\x6f\x69\x6e\x20\x40\x43\x68\x65\x72\x69\x73\x68\x65\x64\x5f\x43\x6f\x6d\x6d\x75\x6e\x69\x74\x79\x20\x46\x6f\x72\x20\x41\x6e\x79\x20\x49\x73\x73\x75\x65\x73"
+    )
     await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("BrandrdXMusic").info("Stopping Branded Music Bot...")
 
 
 if __name__ == "__main__":
-    loop.run_until_complete(init())
-    LOGGER("BrandrdXMusic").info("😢 𝐒𝐨𝐫𝐫𝐲 𝐒𝐭𝐨𝐩𝐩𝐢𝐧𝐠 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 ☹️")
+    asyncio.get_event_loop().run_until_complete(init())
