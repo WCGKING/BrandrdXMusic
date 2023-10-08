30f23feb133faf5df50c06b0eaa4d8a619e158ee
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-08 12:37:40 +0530

    BrandrdXMusic

--- config.py
+++ config.py
@@ -21,9 +21,9 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1001859615470"))
 
-# Get this value from @PiyushXManagerBot on Telegram by /id
+# Get this value from @BRANDRD_ROBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6258877205))
 
 ## Fill these variables if you're deploying on heroku.
