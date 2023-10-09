905b49499575fd34cfee7c26c06d26e8c6826daf
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-09 22:55:20 +0530

    BrandrdXMusic

---
+++ BrandrdXMusic/utils/inline/start.py
@@ -0,0 +1,39 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from BrandrdXMusic import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
+        ],
+    ]
+    return buttons
