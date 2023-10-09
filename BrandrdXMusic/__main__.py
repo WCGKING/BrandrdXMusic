2b1ccb847e66bf8e1772c1ce5d30a83ce5c9c434
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-09 12:26:09 +0530

    Update __main__.py

--- BrandrdXMusic/__main__.py
+++ BrandrdXMusic/__main__.py
@@ -5,7 +5,8 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from BrandrdXMusic import LOGGER, app, userbot
+from BrandrdXMusic import app, userbot
+from .logging import LOGGER
 from BrandrdXMusic.core.call import Hotty
 from BrandrdXMusic.misc import sudo
 from BrandrdXMusic.plugins import ALL_MODULES
