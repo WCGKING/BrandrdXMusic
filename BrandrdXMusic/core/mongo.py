e25e24508121a9a81d0156897d760c5030450573
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-09 12:02:39 +0530

    Update mongo.py

--- BrandrdXMusic/core/mongo.py
+++ BrandrdXMusic/core/mongo.py
@@ -12,7 +12,7 @@ TEMP_MONGODB = ""
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning("No MONGO DB URL found. LOL")
     temp_client = Client(
-        "Valencia",
+        "Brandrd",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
@@ -28,5 +28,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Valencia
-    pymongodb = _mongo_sync_.Valencia
+    mongodb = _mongo_async_.Brandrd
+    pymongodb = _mongo_sync_.Brandrd
