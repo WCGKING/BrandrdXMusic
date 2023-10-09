3201e7377db7a3c6c56c53a4c584ba7e9fd79097
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-09 20:39:31 +0530

    Create __init__.py

---
+++ BrandrdXMusic/__init__.py
@@ -0,0 +1,26 @@
+from BrandrdXMusic.core.bot import Hotty
+from BrandrdXMusic.core.dir import dirr
+from BrandrdXMusic.core.git import git
+from BrandrdXMusic.core.userbot import Userbot
+from BrandrdXMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Hotty()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
