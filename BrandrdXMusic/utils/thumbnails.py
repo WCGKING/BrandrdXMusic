664ab9d177613833d56f6ce159d9abc571efc41d
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-10 07:18:28 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/thumbnails.py
+++ BrandrdXMusic/utils/thumbnails.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from ValenciaXMusic import app
+from BrandrdXMusic import app
 from config import YOUTUBE_IMG_URL
 
 
@@ -71,8 +71,8 @@ async def get_thumb(videoid):
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.5)
         draw = ImageDraw.Draw(background)
-        arial = ImageFont.truetype("ValenciaXMusic/assets/font2.ttf", 30)
-        font = ImageFont.truetype("ValenciaXMusic/assets/font.ttf", 30)
+        arial = ImageFont.truetype("BrandrdXMusic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("BrandrdXMusic/assets/font.ttf", 30)
         draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
         draw.text(
             (55, 560),
