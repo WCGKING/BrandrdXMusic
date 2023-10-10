4bbcb045746f7da09275d0cfd3e3a632477a75f1
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-10 07:08:42 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/database.py
+++ BrandrdXMusic/utils/database.py
@@ -1,8 +1,8 @@
 import random
 from typing import Dict, List, Union
 
-from ValenciaXMusic import userbot
-from ValenciaXMusic.core.mongo import mongodb
+from BrandrdXMusic import userbot
+from BrandrdXMusic.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -213,7 +213,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from ValenciaXMusic.core.userbot import assistants
+    from BrandrdXMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -227,7 +227,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from ValenciaXMusic.core.userbot import assistants
+    from BrandrdXMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -254,7 +254,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from ValenciaXMusic.core.userbot import assistants
+    from BrandrdXMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -267,7 +267,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from ValenciaXMusic.core.userbot import assistants
+    from BrandrdXMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
