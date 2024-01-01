a2eee797485c47d22deef6be2c232842d435d1a2
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-01 19:02:20 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
