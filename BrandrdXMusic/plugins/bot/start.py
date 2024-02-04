0f4ec46e8741f87ac00264c9885c5ccfe00d5c3b
Prince <153365616+Kurama-fox@users.noreply.github.com>
2024-02-04 19:54:21 +0530

    Update start.py
    pp

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_video(
-                       video="https://graph.org/file/8fe2b8564471a468f4d19.mp4",
+                       video="https://cdn.pixabay.com/photo/2023/01/22/11/49/girl-7736189_1280.jpg",
                        caption=_["help_1"].format(config.SUPPORT_CHAT), reply_markup=keyboard
             )
         if name[0:3] == "sud":
@@ -84,7 +84,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_video(
-            video="https://telegra.ph//file/5374701ae0678848e9631.mp4",
+            video="https://cdn.pixabay.com/photo/2023/01/22/11/49/girl-7736189_1280.jpg",
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
