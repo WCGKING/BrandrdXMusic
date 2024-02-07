792d8738762ee0e97bf37751bcdba3281b72c1a8
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-02-07 17:24:35 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/bot/settings.py
+++ BrandrdXMusic/plugins/bot/settings.py
@@ -54,7 +54,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://te.legra.ph/file/13d410c5ae225e7ff5ec8.mp4"),
+        InputMediaVideo("https://te.legra.ph/file/ef47f077b671f69f8d8f0.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
