87a465646f4d000683fc7c5b220d64f34720c978
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-02-08 19:05:55 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/admins/tagall.py
+++ BrandrdXMusic/plugins/admins/tagall.py
@@ -154,7 +154,7 @@ VC_TAG = [ "**𝐎𝚈𝙴 𝐕𝙲 𝐀𝙰𝙾 𝐍𝙰 𝐏𝙻𝚂🥲**",
         ]
 
 
-@app.on_message(filters.command(["tagall""], prefixes=["/", "@", "#"]))
+@app.on_message(filters.command(["tagall"], prefixes=["/", "@", ".", "#"]))
 async def mentionall(client, message):
     chat_id = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
