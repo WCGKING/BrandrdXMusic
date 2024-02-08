73a3adb54c5725a53becf163d3767db4bcc7d5e0
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-02-08 18:49:59 +0530

    Create admin_check.py

---
+++ BrandrdXMusic/utils/admin_check.py
@@ -0,0 +1,29 @@
+from pyrogram.types import Message
+from pyrogram.enums import ChatType, ChatMemberStatus
+
+
+async def admin_check(message: Message) -> bool:
+    if not message.from_user:
+        return False
+
+    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL]:
+        return False
+
+    if message.from_user.id in [
+        777000,  # Telegram Service Notifications
+        1087968824,  # GroupAnonymousBot
+    ]:
+        return True
+
+    client = message._client
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    check_status = await client.get_chat_member(chat_id=chat_id, user_id=user_id)
+    if check_status.status not in [
+        ChatMemberStatus.OWNER,
+        ChatMemberStatus.ADMINISTRATOR
+    ]:
+        return False
+    else:
+        return True
