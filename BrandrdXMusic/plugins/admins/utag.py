70d233eed0b2e41a537542533ae672fee91ba8d6
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-02-08 15:49:16 +0530

    Update utag.py

--- BrandrdXMusic/plugins/admins/utag.py
+++ BrandrdXMusic/plugins/admins/utag.py
@@ -10,7 +10,7 @@ from BrandrdXMusic import app
 
 SPAM_CHATS = {}
 
-@app.on_message(filters.command(["utag", "uall"], prefixes=["/", "@", ".", "#"]) & admin_filter)
+@app.on_message(filters.command(["utag", "uall"], prefixes=["/", "@", ".", "#"]) 
 async def tag_all_users(_, message):
     global SPAM_CHATS
     chat_id = message.chat.id
