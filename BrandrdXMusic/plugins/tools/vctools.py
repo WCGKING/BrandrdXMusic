9df78f7d8bc33958d5c04af902ae55e390994de0
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-02-11 13:42:56 +0530

    BrandedXMusic

--- BrandrdXMusic/plugins/tools/vctools.py
+++ BrandrdXMusic/plugins/tools/vctools.py
@@ -18,7 +18,7 @@ async def brah3(app :app, message:Message):
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
-               text += f"[{user.first_name}] "
+               text += f"[{user.first_name}](tg://user?id={user.id}) "
                x += 1
              except Exception:
                pass
