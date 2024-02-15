fd5e23e75aeadda2fca8fd8c721d71af94f19e66
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-02-15 15:00:58 +0530

    BrandrdXMusic

---
+++ BrandrdXMusic/mongo/couples_db.py
@@ -0,0 +1,36 @@
+from BrandrdXMusic.utils.mongo import db
+
+coupledb = db.couple
+
+async def _get_lovers(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
+
+async def _get_image(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["img"]
+    else:
+        lovers = {}
+    return lovers
+
+async def get_couple(cid: int, date: str):
+    lovers = await _get_lovers(cid)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_couple(cid: int, date: str, couple: dict, img: str):
+    lovers = await _get_lovers(cid)
+    lovers[date] = couple
+    await coupledb.update_one(
+        {"chat_id": cid},
+        {"$set": {"couple": lovers, "img": img}},
+        upsert=True,
+                              )
