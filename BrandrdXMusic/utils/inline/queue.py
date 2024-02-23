56e1eec8eba15d359cc7c4b0bb2fbd83acff811c
honey-cracker <160338822+honey-cracker@users.noreply.github.com>
2024-02-23 22:17:52 +0530

    Update queue.py

--- BrandrdXMusic/utils/inline/queue.py
+++ BrandrdXMusic/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/BRANDEDKING82"
+                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/Slayer_Honey"
             ),
             InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/Honey_slayer_01"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
