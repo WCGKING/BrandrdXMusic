a69071bafd725896899d72664e582771646b2264
Suryan008 <157453764+Suryan008@users.noreply.github.com>
2024-02-23 10:19:09 +0530

    Update settings.py

--- BrandrdXMusic/plugins/bot/settings.py
+++ BrandrdXMusic/plugins/bot/settings.py
@@ -54,7 +54,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://te.legra.ph/file/ef47f077b671f69f8d8f0.mp4"),
+        InputMediaVideo("https://telegra.ph/file/b41c6ecdfb1a433febe8f.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
