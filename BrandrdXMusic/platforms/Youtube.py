dfde83a5d945f3db043acde569d8e2b2dd29b155
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-07 22:59:38 +0530

    BrandrdXMusic

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -2,6 +2,8 @@ import asyncio
 import os
 import re
 from typing import Union
+from pytgcalls.types.input_stream import InputStream
+from pytgcalls.types.input_stream import InputAudioStream
 
 import yt_dlp
 from pyrogram.enums import MessageEntityType
