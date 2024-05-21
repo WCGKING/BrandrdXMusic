de74a61e0bf4cd09b73ddadff4c9878c0fae7c7f
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-21 12:33:04 +0530

    Update call.py

--- BrandrdXMusic/core/call.py
+++ BrandrdXMusic/core/call.py
@@ -9,7 +9,7 @@ from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
-    TelegramServerError,
+    pytgcalls.exceptions,
 )
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
