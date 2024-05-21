f0275b044f8cea98733f6b2528bfdcbefc1c4906
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-21 13:06:43 +0530

    BrandrdXMusic

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -243,7 +243,7 @@ class YouTubeAPI:
 
         def audio_dl():
             ydl_optssx = {
-                "format": "bestaudio/[ext=m4a]",
+                "format": "bestaudio/best",
                 "outtmpl": "downloads/%(id)s.%(ext)s",
                 "geo_bypass": True,
                 "nocheckcertificate": True,
