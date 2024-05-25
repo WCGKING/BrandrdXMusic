bb3197374a5bfaabd0d3e3bacfdbb992ca627a3b
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-25 12:25:50 +0530

    BrandrdXMusic

---
+++ BrandrdXMusic/utils/events.py
@@ -0,0 +1,23 @@
+import inspect
+import re
+from pathlib import Path
+from telethon import events
+from BrandrdXMusic import telethn
+
+
+def register(**args):
+    """Registers a new message."""
+    pattern = args.get("pattern", None)
+
+    r_pattern = r"^[/!.]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.NewMessage(**args))
+        return func
+
+    return decorator
