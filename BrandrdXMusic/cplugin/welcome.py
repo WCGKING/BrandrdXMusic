1ba4e5afbde99db1820571ccc9a00bb5d013350d
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-25 23:15:58 +0530

    BrandrdXMusic

--- BrandrdXMusic/cplugin/welcome.py
+++ BrandrdXMusic/cplugin/welcome.py
@@ -42,11 +42,11 @@ from logging import getLogger
 
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://graph.org/file/e36ca3c73f08d4c04b9b6.jpg",
+    "https://graph.org/file/970955ce30672302943c1.jpg",
+    "https://graph.org/file/e38d003cff04e6dffb5b2.jpg",
+    "https://graph.org/file/98f6187aa4d3d3954ccdc.jpg",
+    "https://graph.org/file/f4b34351a59061ba1c61b.jpg",
 ]
 # --------------------------------------------------------------------------------- #
 
