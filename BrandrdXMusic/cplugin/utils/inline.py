2f814eedf3aa558c9400f4f208e600240519765d
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-25 10:28:42 +0530

    BrandrdXMusic

---
+++ BrandrdXMusic/cplugin/utils/inline.py
@@ -0,0 +1,16 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+buttons = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="▷", callback_data="resume_cb"),
+            InlineKeyboardButton(text="II", callback_data="pause_cb"),
+            InlineKeyboardButton(text="‣‣I", callback_data="skip_cb"),
+            InlineKeyboardButton(text="▢", callback_data="end_cb"),
+        ]
+    ]
+)
+
+close_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="✯ ᴄʟᴏsᴇ ✯", callback_data="close")]]
+)
