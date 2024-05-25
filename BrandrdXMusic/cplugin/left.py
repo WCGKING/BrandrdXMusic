fbcdedf089fe8376d43cbd4b4524141da3a1000f
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-25 13:40:47 +0530

    BrandrdXMusic

--- BrandrdXMusic/cplugin/left.py
+++ BrandrdXMusic/cplugin/left.py
@@ -19,7 +19,7 @@ from pyrogram.enums import ParseMode
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from BrandrdXMusic.utils.Hotty_ban import admin_filter
+from BrandrdXMusic.utils.branded_ban import admin_filter
 from BrandrdXMusic import app
 from pyrogram import filters
 from pyrogram.errors import RPCError
