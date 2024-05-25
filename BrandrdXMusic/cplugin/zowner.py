9925f14e4e28dfaa2b1225f9cae66d1c4930ddf3
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-25 15:38:18 +0530

    BrandrdXMusic

--- BrandrdXMusic/cplugin/zowner.py
+++ BrandrdXMusic/cplugin/zowner.py
@@ -125,7 +125,7 @@ async def add_all(client, message):
         lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
 
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1001991616368:
+            if dialog.chat.id == -1002198719573:
                 continue
             try:
                 await userbot.add_chat_members(dialog.chat.id, app_id)
