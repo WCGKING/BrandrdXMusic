27b6f0f6073969c07b299658f4278b5a596f09b1
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-25 12:36:08 +0530

    BrandrdXMusic

--- BrandrdXMusic/logging.py
+++ BrandrdXMusic/logging.py
@@ -13,6 +13,8 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+logging.getLogger("ntgcalls").setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:
