bbe80f90b07fa492d48a8b266b97edfe2d06255c
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-27 14:19:07 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -27,7 +27,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def stream_markup_timer(_, chat_id, played, dur):
+def stream_markup_timer(_, vidid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
