8a826aedd1342eaa1f2e800bb8f7ce9a2c91202f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-27 14:42:32 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -3,7 +3,7 @@ import math
 from pyrogram.types import InlineKeyboardButton
 
 from BrandrdXMusic.utils.formatters import time_to_seconds
-
+from BrandrdXMusic import app
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
