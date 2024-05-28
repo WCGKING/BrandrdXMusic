62a3ad03539892a0b1df834bb52c7036b641b7ba
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-28 12:11:36 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/tools/vctools.py
+++ BrandrdXMusic/plugins/tools/vctools.py
@@ -1,8 +1,19 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from BrandrdXMusic import app
+from pyrogram import *
+from pyrogram.types import *
 from config import OWNER_ID
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
+from pyrogram.raw.types import InputGroupCall
+from BrandrdXMusic.utils.database import get_assistant
+from telethon.tl.functions.phone import (
+    CreateGroupCallRequest,
+    DiscardGroupCallRequest,
+    GetGroupCallRequest,
+    InviteToGroupCallRequest,
+)
 
 
 # vc on
