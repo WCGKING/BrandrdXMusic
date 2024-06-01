acda0621b9247776fa734ca53392ac134237ca95
Bugsirtg <bugsirtg@gmail.com>
2024-06-01 14:11:51 +0530

    Update all_tag.py

--- BrandrdXMusic/plugins/admins/all_tag.py
+++ BrandrdXMusic/plugins/admins/all_tag.py
@@ -31,7 +31,7 @@ async def tag_all_users(_, message):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 5
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"\n⊚ [{m.user.first_name}]\n"
             if usernum == 1:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(2)
@@ -51,11 +51,11 @@ async def tag_all_users(_, message):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"\n⊚ [{m.user.first_name}]\n"
             if usernum == 5:
                 await app.send_message(
                     message.chat.id,
-                    f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
+                    f"{text}\n{usertxt}\n\n|| ➥ ᴇᴅᴀ ᴍᴏɴᴇ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
                 )
                 await asyncio.sleep(2)
                 usernum = 0
@@ -92,8 +92,8 @@ async def cancelcmd(_, message):
             SPAM_CHATS.remove(chat_id)
         except Exception:
             pass
-        return await message.reply_text("**ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")
+        return await message.reply_text("** ᴇᴅᴀ ᴍᴏɴᴇ ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")
 
     else:
-        await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")
+        await message.reply_text("** ᴇᴅᴀ ᴍᴏɴᴇ ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")
         return
