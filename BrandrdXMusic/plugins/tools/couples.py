fafef4432619b7f5048d4962aba9f6c4b938c80c
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-06-08 22:39:09 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/tools/couples.py
+++ BrandrdXMusic/plugins/tools/couples.py
@@ -1,155 +1,111 @@
-import os 
+import os
 import random
-from datetime import datetime 
-from telegraph import upload_file
-from PIL import Image , ImageDraw
-from pyrogram import *
-from pyrogram.types import *
-from pyrogram.enums import *
 
-#BOT FILE NAME
-from BrandrdXMusic import app as app
-from BrandrdXMusic.mongo.couples_db import _get_image, get_couple
+from PIL import Image, ImageDraw
+from pyrogram import filters
+from pyrogram.enums import ChatAction, ChatType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-POLICE = [
-    [
-        InlineKeyboardButton(
-            text="𓊈❥[𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚 ]❥𓊉",
-            url=f"https://t.me/BRANDEDKING82",
-        ),
-    ],
-]
+from config import OWNER_ID
+from BrandrdXMusic import app
 
 
-def dt():
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M")
-    dt_list = dt_string.split(" ")
-    return dt_list
-    
-
-def dt_tom():
-    a = (
-        str(int(dt()[0].split("/")[0]) + 1)
-        + "/"
-        + dt()[0].split("/")[1]
-        + "/"
-        + dt()[0].split("/")[2]
+@app.on_message(
+    filters.command(
+        ["couples", "couple"],
+        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
     )
-    return a
-
-tomorrow = str(dt_tom())
-today = str(dt()[0])
-
-@app.on_message(filters.command("couples"))
-async def ctest(_, message):
+)
+async def couples(app, message):
     cid = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
-        return await message.reply_text("This command only works in groups.")
+        return await message.reply_text("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘs.")
     try:
-     #  is_selected = await get_couple(cid, today)
-     #  if not is_selected:
-         msg = await message.reply_text("❣️")
-         #GET LIST OF USERS
-         list_of_users = []
+        msg = await message.reply_text("❣️")
+        list_of_users = []
 
-         async for i in app.get_chat_members(message.chat.id, limit=50):
-             if not i.user.is_bot:
-               list_of_users.append(i.user.id)
+        async for i in app.get_chat_members(message.chat.id, limit=50):
+            if not i.user.is_bot and not i.user.is_deleted:
+                list_of_users.append(i.user.id)
 
-         c1_id = random.choice(list_of_users)
-         c2_id = random.choice(list_of_users)
-         while c1_id == c2_id:
-              c1_id = random.choice(list_of_users)
+        c1_id = random.choice(list_of_users)
+        c2_id = random.choice(list_of_users)
+        while c1_id == c2_id:
+            c1_id = random.choice(list_of_users)
 
+        photo1 = (await app.get_chat(c1_id)).photo
+        photo2 = (await app.get_chat(c2_id)).photo
 
-         photo1 = (await app.get_chat(c1_id)).photo
-         photo2 = (await app.get_chat(c2_id)).photo
- 
-         N1 = (await app.get_users(c1_id)).mention 
-         N2 = (await app.get_users(c2_id)).mention
-         
-         try:
+        N1 = (await app.get_users(c1_id)).mention
+        N2 = (await app.get_users(c2_id)).mention
+
+        try:
             p1 = await app.download_media(photo1.big_file_id, file_name="pfp.png")
-         except Exception:
+        except Exception:
             p1 = "BrandrdXMusic/assets/upic.png"
-         try:
+        try:
             p2 = await app.download_media(photo2.big_file_id, file_name="pfp1.png")
-         except Exception:
+        except Exception:
             p2 = "BrandrdXMusic/assets/upic.png"
-            
-         img1 = Image.open(f"{p1}")
-         img2 = Image.open(f"{p2}")
+        try:
+            await app.resolve_peer(OWNER_ID[0])
+            OWNER = OWNER_ID[0]
+        except:
+            OWNER = f"tg://openmessage?user_id={OWNER_ID[0]}"
 
-         img = Image.open("BrandrdXMusic/assets/cppicbranded.jpg")
+        img1 = Image.open(f"{p1}")
+        img2 = Image.open(f"{p2}")
 
-         img1 = img1.resize((437,437))
-         img2 = img2.resize((437,437))
+        img = Image.open("BrandrdXMusic/assets/Couple.png")
 
-         mask = Image.new('L', img1.size, 0)
-         draw = ImageDraw.Draw(mask) 
-         draw.ellipse((0, 0) + img1.size, fill=255)
+        img1 = img1.resize((390, 390))
+        img2 = img2.resize((390, 390))
 
-         mask1 = Image.new('L', img2.size, 0)
-         draw = ImageDraw.Draw(mask1) 
-         draw.ellipse((0, 0) + img2.size, fill=255)
+        mask = Image.new("L", img1.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + img1.size, fill=255)
 
+        mask1 = Image.new("L", img2.size, 0)
+        draw = ImageDraw.Draw(mask1)
+        draw.ellipse((0, 0) + img2.size, fill=255)
 
-         img1.putalpha(mask)
-         img2.putalpha(mask1)
+        img1.putalpha(mask)
+        img2.putalpha(mask1)
 
-         draw = ImageDraw.Draw(img)
+        draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (116, 160), img1)
-         img.paste(img2, (789, 160), img2)
+        img.paste(img1, (125, 196), img1)
+        img.paste(img2, (780, 196), img2)
 
-         img.save(f'test_{cid}.png')
-    
-         TXT = f"""
-****ᴛᴏᴅᴀʏ's sᴇʟᴇᴄᴛᴇᴅ ᴄᴏᴜᴘʟᴇs 🌺🎉 :
-➖➖➖➖➖➖➖➖➖➖➖➖
+        img.save(f"test_{cid}.png")
+
+        TXT = f"""
+**ᴛᴏᴅᴀʏ's sᴇʟᴇᴄᴛᴇᴅ ᴄᴏᴜᴘʟᴇs 🌺 :
+
 {N1} + {N2} = ❣️
-➖➖➖➖➖➖➖➖➖➖➖➖
-ɴᴇxᴛ ᴄᴏᴜᴘʟᴇꜱ ᴡɪʟʟ ʙᴇ ꜱᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow} !!**
-"""
-    
-         await message.reply_photo(f"test_{cid}.png", caption=TXT)
-         await msg.delete()
-         a = upload_file(f"test_{cid}.png")
-         for x in a:
-           img = "https://graph.org/" + x
-           couple = {"c1_id": c1_id, "c2_id": c2_id}
-          # await save_couple(cid, today, couple, img)
-    
-         
-      # elif is_selected:
-      #   msg = await message.reply_text("ɢᴇᴛᴛɪɴɢ ᴛᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇs ɪᴍᴀɢᴇ...")
-      #   b = await _get_image(cid)
-       #  c1_id = int(is_selected["c1_id"])
-       #  c2_id = int(is_selected["c2_id"])
-       #  c1_name = (await app.get_users(c1_id)).first_name
-        # c2_name = (await app.get_users(c2_id)).first_name
-         
-      #   TXT = f"""
-#**ᴛᴏᴅᴀʏ's sᴇʟᴇᴄᴛᴇᴅ ᴄᴏᴜᴘʟᴇs 🎉 :
-#➖➖➖➖➖➖➖➖➖➖➖➖
-#[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❣️
-#➖➖➖➖➖➖➖➖➖➖➖➖
-#ɴᴇxᴛ ᴄᴏᴜᴘʟᴇꜱ ᴡɪʟʟ ʙᴇ ꜱᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow} !!**
-#"""
 
- #        await message.reply_photo(b, caption=TXT)
-        # await msg.delete()
+**
+"""
+        await app.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
+        await message.reply_photo(
+            f"test_{cid}.png",
+            caption=TXT,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton(text="ᴍʏ ᴄᴜᴛᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ 🌋", user_id=OWNER)]]
+            ),
+        )
+        await msg.delete()
+        try:
+            os.remove(f"./downloads/pfp1.png")
+            os.remove(f"./downloads/pfp2.png")
+            os.remove(f"test_{cid}.png")
+        except Exception:
+            pass
     except Exception as e:
         print(str(e))
     try:
-      os.remove(f"./downloads/pfp1.png")
-      os.remove(f"./downloads/pfp2.png")
-      os.remove(f"test_{cid}.png")
+        os.remove(f"./downloads/pfp1.png")
+        os.remove(f"./downloads/pfp2.png")
+        os.remove(f"test_{cid}.png")
     except Exception:
-       pass
-
-__mod__ = "COUPLES"
-__help__ = """
-**» /couples** - Get Todays Couples Of The Group In Interactive View
-"""
+        pass
