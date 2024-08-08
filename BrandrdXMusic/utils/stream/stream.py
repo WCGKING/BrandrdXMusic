62fea40189154631d83eca6fd8d02e105bcb71bc
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-08 12:52:26 +0530

    Update stream.py

--- BrandrdXMusic/utils/stream/stream.py
+++ BrandrdXMusic/utils/stream/stream.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 from BrandrdXMusic.utils.database import get_assistant
 import config
-from BrandrdXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from BrandrdXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app, YTB
 from BrandrdXMusic.core.call import Hotty
 from BrandrdXMusic.misc import SUDOERS
 from BrandrdXMusic.utils import seconds_to_min, time_to_seconds
@@ -128,8 +128,12 @@ async def stream(
                         vidid, mystic, video=status, videoid=True
                     )
                 except:
-
-                    await mystic.edit_text(_["play_3"])
+                    try:
+                        file_path, direct = await YTB.download(
+                            vidid, mystic, video=status, videoid=True
+                        )
+                    except:
+                        await mystic.edit_text(_["play_3"])
                 await Hotty.join_call(
                     chat_id,
                     original_chat_id,
@@ -194,8 +198,12 @@ async def stream(
                 vidid, mystic, videoid=True, video=status
             )
         except:
-
-            await mystic.edit_text(_["play_3"])
+            try:
+                file_path, direct = await YouTube.download(
+                    vidid, mystic, videoid=True, video=status
+                )
+            except:
+                await mystic.edit_text(_["play_3"])
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
