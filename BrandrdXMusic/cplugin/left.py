e95b044d5bac2d49873a03d60859c6f591d75246
zerinhogod <luceenavictor@gmail.com>
2024-08-13 02:08:22 -0300

    Update left.py

--- BrandrdXMusic/cplugin/left.py
+++ BrandrdXMusic/cplugin/left.py
@@ -98,8 +98,8 @@ async def member_has_left(client: Client, member: ChatMemberUpdated):
         else:
             welcome_photo = random.choice(random_photo)
 
-        caption = f"**#New_Member_Left**\n\n**๏** {user.mention} **ʜᴀs ʟᴇғᴛ ᴛʜɪs ɢʀᴏᴜᴘ**\n**๏ sᴇᴇ ʏᴏᴜ sᴏᴏɴ ᴀɢᴀɪɴ..!**"
-        button_text = "๏ ᴠɪᴇᴡ ᴜsᴇʀ ๏"
+        caption = f"**#New_Member_Left**\n\n**๏** {user.mention} **saiu dessa merda de grupo**\n**Que não volte nunca mais, brinks...**"
+        button_text = "BISBILHOTAR QUEM ERA"
         deep_link = f"tg://openmessage?user_id={user.id}"
 
         message = await client.send_photo(
