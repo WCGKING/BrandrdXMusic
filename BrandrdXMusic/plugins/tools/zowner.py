b95d452237a4c764499ea29c4c8d2549526c7aaa
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-08-18 21:52:10 +0530

    BrandrdXMusic

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -1,39 +1,14 @@
-from pyrogram import Client, filters
-import requests
-import random
-import os
-import re
 import asyncio
-import time
-from BrandrdXMusic import app
-from BrandrdXMusic.utils.database import add_served_chat, delete_served_chat
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from BrandrdXMusic.utils.database import get_assistant
-import asyncio
-from BrandrdXMusic.misc import SUDOERS
-from BrandrdXMusic.mongo.afkdb import HEHE
-from BrandrdXMusic.core.userbot import Userbot
-from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
-from BrandrdXMusic import app
-import asyncio
-import random
-from pyrogram import Client, filters
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import (
-    ChatAdminRequired,
-    InviteRequestSent,
-    UserAlreadyParticipant,
-    UserNotParticipant,
-)
-from BrandrdXMusic import app
-from BrandrdXMusic.utils.branded_ban import admin_filter
-from BrandrdXMusic.utils.decorators.userbotjoin import UserbotWrapper
-from BrandrdXMusic.utils.database import get_assistant, is_active_chat
 
-@app.on_message(
-    filters.command("rehhggggggvvpo")
-    & filters.group)
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from BrandrdXMusic import app
+from BrandrdXMusic.mongo.afkdb import LOGGERS as OWNERS
+from BrandrdXMusic.utils.database import add_served_chat, get_assistant
+
+
+@app.on_message(filters.command("repo"))
 async def help(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
@@ -42,50 +17,41 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/WCGKING/BrandrdXMusic")
+                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                    )
                 ]
             ]
         ),
     )
 
-@app.on_message(
-    filters.command("revbvvvvvvpo")
-    & filters.group)
-async def help(client: Client, message: Message):
-    userbot = await get_assistant(chat_id)
+
+@app.on_message(filters.command("clone"))
+async def clones(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
-        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        caption=f"""**🙂You Are Not Sudo User So You Are Not Allowed To Clone Me.**\n**😌Click Given Below Button And Host Manually Otherwise Contact Owner Or Sudo Users For Clone.**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/WCGKING/BrandrdXMusic")
+                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                    )
                 ]
             ]
         ),
     )
 
-@app.on_message(
-    filters.command("rhhgggggggepo")
-    & filters.private)
-async def help(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
-        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/WCGKING/BrandrdXMusic")
-                ]
-            ]
-        ),
-    )
 
 # --------------------------------------------------------------------------------- #
 
-@app.on_message(filters.command(["hi", "hii", "hello", "hui", "good", "gm", "ok", "bye", "welcome", "thanks"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group)
+
+@app.on_message(
+    filters.command(
+        ["hi", "hii", "hello", "hui", "good", "gm", "ok", "bye", "welcome", "thanks"],
+        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
+    )
+    & filters.group
+)
 async def bot_check(_, message):
     chat_id = message.chat.id
     await add_served_chat(chat_id)
@@ -94,18 +60,18 @@ async def bot_check(_, message):
 # --------------------------------------------------------------------------------- #
 
 
-
-
 import asyncio
-import time
 
-@app.on_message(filters.command("gadd") & filters.user(int(HEHE)))
-async def add_all(client, message):
+
+@app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
+async def add_allbot(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
-        await message.reply("**⚠️ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @BRANDED_KUDI_BOT`**")
+        await message.reply(
+            "**⚠️ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @BRANDED_KUDI_BOT`**"
+        )
         return
-    
+
     bot_username = command_parts[1]
     try:
         userbot = await get_assistant(message.chat.id)
@@ -114,11 +80,12 @@ async def add_all(client, message):
         done = 0
         failed = 0
         lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
-        
+        await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
             if dialog.chat.id == -1001754457302:
                 continue
             try:
+
                 await userbot.add_chat_members(dialog.chat.id, app_id)
                 done += 1
                 await lol.edit(
@@ -130,9 +97,20 @@ async def add_all(client, message):
                     f"**🔂 ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅɪɴɢ ʙʏ»** @{userbot.username}"
                 )
             await asyncio.sleep(3)  # Adjust sleep time based on rate limits
-        
+
         await lol.edit(
             f"**➻ {bot_username} ʙᴏᴛ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ🎉**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
         )
     except Exception as e:
         await message.reply(f"Error: {str(e)}")
+
+
+__MODULE__ = "Sᴏᴜʀᴄᴇ"
+__HELP__ = """
+## Rᴇᴘᴏ Sᴏᴜʀᴄᴇ Mᴏᴅᴜᴇ
+
+Tʜɪs ᴍᴏᴅᴜᴇ ᴘʀᴏᴠɪᴅᴇs ᴜᴛɪɪᴛʏ ᴄᴏᴍᴍᴀɴᴅs ғᴏʀ ᴜsᴇʀs ᴛᴏ ɪɴᴛᴇʀᴀᴄᴛ ᴡɪᴛʜ ᴛʜᴇ ʙᴏᴛ.
+
+### Cᴏᴍᴍᴀɴᴅs:
+- `/ʀᴇᴘᴏ`: Gᴇᴛ ᴛʜᴇ ɪɴᴋ ᴛᴏ ᴛʜᴇ ʙᴏᴛ's sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʀᴇᴘᴏsɪᴛᴏʀʏ.
+"""
