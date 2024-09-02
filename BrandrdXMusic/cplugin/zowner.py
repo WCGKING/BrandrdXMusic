bbc71d777ae1883444515be5f4dd126b9e977293
Visionx-x <telegrambott19@gmail.com>
2024-09-02 22:22:37 +0530

    Update zowner.py

--- BrandrdXMusic/cplugin/zowner.py
+++ BrandrdXMusic/cplugin/zowner.py
@@ -32,7 +32,7 @@ from BrandrdXMusic.utils.decorators.userbotjoin import UserbotWrapper
 from BrandrdXMusic.utils.database import get_assistant, is_active_chat
 
 
-@Client.on_message(filters.command("repo") & filters.group)
+@Client.on_message(filters.command("King") & filters.group)
 async def repo(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://graph.org/file/462ea653de0a313f8302a.jpg",
@@ -49,7 +49,7 @@ async def repo(client: Client, message: Message):
     )
 
 
-@Client.on_message(filters.command("repo") & filters.group)
+@Client.on_message(filters.command("king") & filters.group)
 async def help(client: Client, message: Message):
 
     await message.reply_photo(
@@ -67,7 +67,7 @@ async def help(client: Client, message: Message):
     )
 
 
-@Client.on_message(filters.command("repo") & filters.private)
+@Client.on_message(filters.command("king") & filters.private)
 async def help(client: Client, message: Message):
     await message.reply_photo(
          photo=f"https://graph.org/file/462ea653de0a313f8302a.jpg",
