60b73a9d89deaef33392a5430d0625b4dd000bc5
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-09-11 16:13:47 +0530

    BrandrdXMusic

--- config.py
+++ config.py
@@ -23,7 +23,6 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002198719573"))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "7250012103"))
