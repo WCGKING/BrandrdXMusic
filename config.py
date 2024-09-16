34905d356f322740ce4cbb8153166958fb36f8f5
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-09-16 23:15:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "1002250250187"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002250250187"))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "5899420759"))
