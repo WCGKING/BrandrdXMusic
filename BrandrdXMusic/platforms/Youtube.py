42eac10cf9bd6c820a0f2436ffa364ea8b11dbab
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-09-20 14:44:01 +0530

    Update Youtube.py

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -399,15 +399,7 @@ class YouTubeAPI:
                     downloaded_file = stdout.decode().split("\n")[0]
                     direct = False
                 else:
-                   file_size = await check_file_size(link)
-                   if not file_size:
-                     print("None file Size")
-                     return
-                   total_size_mb = file_size / (1024 * 1024)
-                   if total_size_mb > 250:
-                     print(f"File size {total_size_mb:.2f} MB exceeds the 100MB limit.")
-                     return None
-                   direct = True
+                  direct = True
                    downloaded_file = await loop.run_in_executor(None, video_dl)
         else:
             direct = True
