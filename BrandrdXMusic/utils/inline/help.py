5db65d04b0ef1f1049593831374c075fbeadb030
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-09-21 20:54:54 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/inline/help.py
+++ BrandrdXMusic/utils/inline/help.py
@@ -4,124 +4,21 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from BrandrdXMusic import app
 
-
-def first_page(_):
-    controll_button = [
-        InlineKeyboardButton(text="๏ ᴍᴇɴᴜ ๏", callback_data=f"settingsback_helper"),
-        InlineKeyboardButton(text="๏ ɴᴇxᴛ ๏", callback_data=f"dilXaditi"),
-    ]
-    first_page_menu = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_1"], callback_data="help_callback hb1"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"], callback_data="help_callback hb2"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"], callback_data="help_callback hb3"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_4"], callback_data="help_callback hb4"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_12"], callback_data="help_callback hb12"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"], callback_data="help_callback hb5"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_6"], callback_data="help_callback hb6"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_10"], callback_data="help_callback hb10"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_8"], callback_data="help_callback hb8"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"], callback_data="help_callback hb9"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_11"], callback_data="help_callback hb11"
-                )
-            ],
-            controll_button,
-        ]
-    )
-    return first_page_menu
-
-
-def second_page(_):
-    controll_button = [
-        InlineKeyboardButton(text="๏ ʙᴀᴄᴋ ๏", callback_data=f"settings_back_helper")
-    ]
-    second_page_menu = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_7"], callback_data="help_callback hb7"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_19"], callback_data="help_callback hb19"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"], callback_data="help_callback hb14"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_15"], callback_data="help_callback hb15"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_16"], callback_data="help_callback hb16"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_17"], callback_data="help_callback hb17"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_18"], callback_data="help_callback hb18"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_13"], callback_data="help_callback hb13"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_20"], callback_data="help_callback hb20"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_22"], callback_data="help_callback hb22"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_21"], callback_data="help_callback hb21"
-                )
-            ],
-            controll_button,
-        ]
-    )
-    return second_page_menu
     
 
 def help_pannel(_, START: Union[bool, int] = None):
     first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
     second = [
         InlineKeyboardButton(
+            text=_["BACK_PAGE"],
+            callback_data=f"mbot_cb",
+        ),
+        InlineKeyboardButton(
             text=_["BACK_BUTTON"],
             callback_data=f"settingsback_helper",
+        ),InlineKeyboardButton(
+            text=_["NEXT_PAGE"],
+            callback_data=f"mbot_cb",
         ),
     ]
     mark = second if START else first
