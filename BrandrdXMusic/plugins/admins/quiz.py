a94a2750edf699a514663a5038fab620e0ca56ee
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-09-21 20:40:46 +0530

    BrandrdXMusic

---
+++ BrandrdXMusic/plugins/admins/quiz.py
@@ -0,0 +1,48 @@
+import random
+import requests
+import time
+
+from pyrogram import filters
+from pyrogram.enums import PollType, ChatAction
+from BrandrdXMusic import app
+
+
+last_command_time = {}
+
+
+@app.on_message(filters.command(["quiz"]))
+async def quiz(client, message):
+    user_id = message.from_user.id
+    current_time = time.time()
+
+    if user_id in last_command_time and current_time - last_command_time[user_id] < 5:
+        await message.reply_text(
+            "Pʟᴇᴀsᴇ ᴡᴀɪᴛ 𝟻 sᴇᴄᴏɴᴅs ʙᴇғᴏʀᴇ ᴜsɪɴɢ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴀɢᴀɪɴ."
+        )
+        return
+
+    last_command_time[user_id] = current_time
+
+    categories = [9, 17, 18, 20, 21, 27]
+    await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+
+    url = f"https://opentdb.com/api.php?amount=1&category={random.choice(categories)}&type=multiple"
+    response = requests.get(url).json()
+
+    question_data = response["results"][0]
+    question = question_data["question"]
+    correct_answer = question_data["correct_answer"]
+    incorrect_answers = question_data["incorrect_answers"]
+
+    all_answers = incorrect_answers + [correct_answer]
+    random.shuffle(all_answers)
+
+    cid = all_answers.index(correct_answer)
+    await app.send_poll(
+        chat_id=message.chat.id,
+        question=question,
+        options=all_answers,
+        is_anonymous=False,
+        type=PollType.QUIZ,
+        correct_option_id=cid,
+    )
