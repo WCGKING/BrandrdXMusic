8e3a221420d5c1341ebb51cb2f4936bca6c09d23
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-09-21 21:02:25 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/pastebin.py
+++ BrandrdXMusic/utils/pastebin.py
@@ -1,21 +1,24 @@
-import aiohttp
+import socket,requests
+from asyncio import get_running_loop
+from functools import partial
 
-BASE = "https://batbin.me/"
 
-
-async def post(url: str, *args, **kwargs):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, *args, **kwargs) as resp:
-            try:
-                data = await resp.json()
-            except Exception:
-                data = await resp.text()
+def _netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096).decode("utf-8").strip("\n\x00")
+        if not data:
+            break
         return data
+    s.close()
 
 
-async def HottyBin(text):
-    resp = await post(f"{BASE}api/v2/paste", data=text)
-    if not resp["success"]:
-        return
-    link = BASE + resp["message"]
+async def HottyBin(content):
+    url ="https://pastebin.com/api/api_post.php"
+    data = {"api_dev_key":"9Rfu50iV5l3EuRWATw7EDLuC37RED-C4","api_paste_code": content,"api_option": "paste"}
+    response = requests.post(url, data=data)
+    link=response.text
     return link
