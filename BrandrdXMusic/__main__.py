78878d192c08fbce8a206a08b53d581d9d8ef820
🍷✦ 𝐅𝐀𝐍𝐓𝐀𝐒𝐈𝐄𝐒𝐓 ✦🍷 <faro61867@gmail.com>
2025-03-12 04:08:51 +0530

    Update __main__.py

--- BrandrdXMusic/__main__.py
+++ BrandrdXMusic/__main__.py
@@ -5,11 +5,11 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from BrandrdXMusic import LOGGER, app, userbot
-from BrandrdXMusic.core.call import Hotty
-from BrandrdXMusic.misc import sudo
-from BrandrdXMusic.plugins import ALL_MODULES
-from BrandrdXMusic.utils.database import get_banned_users, get_gbanned
+from Princess-Music import LOGGER, app, userbot
+from Princess-Music.core.call import Hotty
+from Princess-Music.misc import sudo
+from Princess-Music.plugins import ALL_MODULES
+from Princess-Music.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
 
@@ -35,27 +35,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("BrandrdXMusic.plugins" + all_module)
-    LOGGER("BrandrdXMusic.plugins").info("Successfully Imported Modules...")
+        importlib.import_module("Princess-Music.plugins" + all_module)
+    LOGGER("Princess-Music.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Hotty.start()
     try:
         await Hotty.stream_call("https://graph.org/file/e999c40cb700e7c684b75.mp4")
     except NoActiveGroupCall:
-        LOGGER("BrandrdXMusic").error(
+        LOGGER("Princess-Music").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
     await Hotty.decorators()
-    LOGGER("BrandrdXMusic").info(
-        "ᴅʀᴏᴘ ʏᴏᴜʀ ɢɪʀʟꜰʀɪᴇɴᴅ'ꜱ ɴᴜᴍʙᴇʀ ᴀᴛ @BRANDED_PAID_CC ᴊᴏɪɴ @BRANDRD_BOT , @BRANDED_WORLD ꜰᴏʀ ᴀɴʏ ɪꜱꜱᴜᴇꜱ"
+    LOGGER("Princess-Music").info(
+        "ᴅʀᴏᴘ ʏᴏᴜʀ ɢɪʀʟꜰʀɪᴇɴᴅ'ꜱ ɴᴜᴍʙᴇʀ ᴀᴛ 
+ ᴊᴏɪɴ@FANTASIEST_SUPPORTꜰᴏʀ ᴀɴʏ ɪꜱꜱᴜᴇꜱ"@FANTASIEST_PRINCE"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("BrandrdXMusic").info("Stopping Brandrd Music Bot...")
+    LOGGER("Princess-Music").info("Stopping Princess Music Bot...")
 
 
 if __name__ == "__main__":
