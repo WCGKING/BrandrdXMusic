e38319c2262dad90ae62d882a209791e696ab3fe
🍷✦ 𝐅𝐀𝐍𝐓𝐀𝐒𝐈𝐄𝐒𝐓 ✦🍷 <faro61867@gmail.com>
2025-03-12 04:51:47 +0530

    Update queue.py

--- BrandrdXMusic/utils/stream/queue.py
+++ BrandrdXMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 from pyrogram import Client, client
-from BrandrdXMusic.misc import db
-from BrandrdXMusic.utils.formatters import check_duration, seconds_to_min
+from PrincessMusic.misc import db
+from Princess Music.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
