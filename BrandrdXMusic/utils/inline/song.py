a968b31cffb63784edf8d150d0f8430270b635fa
🍷✦ 𝐅𝐀𝐍𝐓𝐀𝐒𝐈𝐄𝐒𝐓 ✦🍷 <faro61867@gmail.com>
2025-03-12 08:32:36 +0530

    Update song.py

--- BrandrdXMusic/utils/inline/song.py
+++ BrandrdXMusic/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_CHAT}",
+                text="🍹 sᴜᴩᴩᴏʀᴛ 🍹", url=f"{config.SUPPORT_CHAT}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
