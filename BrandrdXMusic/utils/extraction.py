3671019f258b6b52dcd45602122855d087b7c88d
🍷✦ 𝐅𝐀𝐍𝐓𝐀𝐒𝐈𝐄𝐒𝐓 ✦🍷 <faro61867@gmail.com>
2025-03-13 14:17:02 +0530

    Update extraction.py

--- BrandrdXMusic/utils/extraction.py
+++ BrandrdXMusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from BrandrdXMusic import app
+from PrincessMusic import app
 
 
 async def extract_user(m: Message) -> User:
