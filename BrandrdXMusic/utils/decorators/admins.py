0c90b887d9a27e014632e2044ba1ae006a5e4185
🍷✦ 𝐅𝐀𝐍𝐓𝐀𝐒𝐈𝐄𝐒𝐓 ✦🍷 <faro61867@gmail.com>
2025-03-13 14:34:52 +0530

    Update admins.py

--- BrandrdXMusic/utils/decorators/admins.py
+++ BrandrdXMusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from BrandrdXMusic import app
-from BrandrdXMusic.misc import SUDOERS, db
-from BrandrdXMusic.utils.database import (
+from PrincessMusic import app
+from PrincessMusic.misc import SUDOERS, db
+from PrincessMusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
