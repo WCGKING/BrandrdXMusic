fb7766cc1e5cf426cb2de19c8f931ebfcf24e32a
🍷✦ 𝐅𝐀𝐍𝐓𝐀𝐒𝐈𝐄𝐒𝐓 ✦🍷 <faro61867@gmail.com>
2025-03-13 13:27:54 +0530

    Update bot.py

--- BrandrdXMusic/core/bot.py
+++ BrandrdXMusic/core/bot.py
@@ -10,7 +10,7 @@ class Hotty(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="BrandrdXMusic",
+            name="Princess-Music",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
