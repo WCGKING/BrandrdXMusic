5db026dc69d09d09b923219e5c3dc2f43196aa61
ANURAG X MUSIC 💞 <lokendrhopal@gmail.com>
2025-05-15 12:25:46 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -70,8 +70,8 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
             InlineKeyboardButton(
                 text="🍭𝐎𝐖𝐍𝐄𝐑 🍭", url=f"https://t.me/ANURAGMOD"
             ),
-            
         ],
+        [InlineKeyboardButton(text="🥂  ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🥂", url=f"https://t.me/MR_BEAST_MUSIC_BOT?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
