3d67b8550026e60aec899371c7684bb41cb053c4
naynay111222333 <tunlinnay1234@gmail.com>
2025-05-28 10:10:50 +0630

    Update vctools.py

--- BrandrdXMusic/plugins/tools/vctools.py
+++ BrandrdXMusic/plugins/tools/vctools.py
@@ -19,13 +19,13 @@ from telethon.tl.functions.phone import (
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-    await msg.reply("**😍ᴠɪᴅᴇᴏ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ🥳**")
+    await msg.reply("**😍Music Date ကြရအောင်🥳**")
 
 
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-    await msg.reply("**😕ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ💔**")
+    await msg.reply("**ဒီနေ့တော့ ဒီလောက်နဲ့နားကြစို့နော် Bye Bye ❤️**")
 
 
 # invite members on vc
