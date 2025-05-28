301b436b57649d3369c1e94df7c9617b497d3aeb
naynay111222333 <tunlinnay1234@gmail.com>
2025-05-28 09:35:05 +0630

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "6599925290"))
+OWNER_ID = int(getenv("OWNER_ID", "7611596091"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/melody_cha")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/melody_cchat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/lifewithmusic4")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/bshmusic12")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -124,4 +124,4 @@ if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
-        )
+)
