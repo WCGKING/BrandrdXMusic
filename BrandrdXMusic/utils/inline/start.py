a7c5a92e7dc973ae638dda8f4d2bcb5873085237
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-09-14 19:08:38 +0530

    Update start.py

--- BrandrdXMusic/utils/inline/start.py
+++ BrandrdXMusic/utils/inline/start.py
@@ -32,8 +32,5 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
-        ],
     ]
     return buttons
