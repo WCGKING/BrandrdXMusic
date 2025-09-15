a1f1c8a793d011094bcfaab138707447871592be
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-09-15 14:58:00 +0530

    BrandrdXMusic

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDRD_BOT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDED_PAID_CC")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BRANDED_WORLD")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
