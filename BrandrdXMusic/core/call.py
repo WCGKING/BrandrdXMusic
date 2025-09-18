a2811a84e80ce45b835a974076ff9cc832003724
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-09-18 20:38:37 +0530

    BrandrdXMusic

--- BrandrdXMusic/core/call.py
+++ BrandrdXMusic/core/call.py
@@ -36,7 +36,7 @@ from BrandrdXMusic.utils.database import (
 )
 from BrandrdXMusic.utils.exceptions import AssistantErr
 from BrandrdXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
-from BrandrdXMusic.utils.inline.play import stream_markup, stream_markup2
+from BrandrdXMusic.utils.inline.play import stream_markup
 from BrandrdXMusic.utils.stream.autoclear import auto_clean
 from BrandrdXMusic.utils.thumbnails import get_thumb
 from strings import get_string
