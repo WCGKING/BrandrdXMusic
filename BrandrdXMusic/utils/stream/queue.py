875e5003ff16cd047881e42a7d3a3a8c38556869
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-09-18 20:34:51 +0530

    BrandrdXMusic

--- BrandrdXMusic/utils/stream/queue.py
+++ BrandrdXMusic/utils/stream/queue.py
@@ -1,6 +1,6 @@
 import asyncio
 from typing import Union
-from pyrogram import Client, client
+
 from BrandrdXMusic.misc import db
 from BrandrdXMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
@@ -88,4 +88,4 @@ async def put_queue_index(
             db[chat_id] = []
             db[chat_id].append(put)
     else:
-        db[chat_id].append(put)
+        db[chat_id].append(put
