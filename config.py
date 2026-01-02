527814e6c9452151419030467375ad2ad711e2d6
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2026-01-02 16:16:09 +0530

    BrandrdXMusic

--- config.py
+++ config.py
@@ -29,6 +29,10 @@ OWNER_ID = int(getenv("OWNER_ID", "7250012103"))
 
 LOG = int(getenv("LOG", True))
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz')
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots6c30dd')
+
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
