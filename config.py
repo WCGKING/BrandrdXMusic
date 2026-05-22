98d14e57b6812cbe1501ddf4c872cc219aa59f99
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2026-05-22 12:41:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ LOG = int(getenv("LOG", True))
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz')
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", '30DxNexGenBots431c89)
+API_KEY = getenv("API_KEY", '30DxNexGenBots431c89')
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
