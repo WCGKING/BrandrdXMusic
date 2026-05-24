2629494b723e66d08d8ad9861dbab9d47d9b7d48
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2026-05-24 15:04:54 +0530

    Update alive.py

--- BrandrdXMusic/plugins/tools/alive.py
+++ BrandrdXMusic/plugins/tools/alive.py
@@ -4,7 +4,7 @@ from BrandrdXMusic import app
 from pyrogram import filters
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from config import MUSIC_BOT_NAME
+
 
 @app.on_message(filters.command(["alive"]))
 async def start(client: Client, message: Message):
