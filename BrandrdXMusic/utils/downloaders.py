818c98590985f0af4a8e0b2210a1fc89e50a8250
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2026-05-24 15:43:40 +0530

    Create downloaders.py

---
+++ BrandrdXMusic/utils/downloaders.py
@@ -0,0 +1,29 @@
+import os
+from yt_dlp import YoutubeDL
+
+ydl_opts = {
+    "format": "bestaudio/best",
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+    "geo_bypass": True,
+    "nocheckcertificate": True,
+    "quiet": True,
+    "no_warnings": True,
+    "prefer_ffmpeg": True,
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "320",
+        }
+    ],
+}
+ydl = YoutubeDL(ydl_opts)
+
+
+def audio_dl(url: str) -> str:
+    sin = ydl.extract_info(url, False)
+    x_file = os.path.join("downloads", f"{sin['id']}.mp3")
+    if os.path.exists(x_file):
+        return x_file
+    ydl.download([url])
+    return x_file
