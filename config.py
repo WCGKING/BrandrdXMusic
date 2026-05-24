8332146bd81419aacef1f5c6f9e44886ae02247b
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2026-05-24 23:23:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ LOG = int(getenv("LOG", True))
 
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint
-YT_API_KEY = getenv("YT_API_KEY" , "xbit_me1MgUhJhrDfkcDFCt4WgNRgucI9kqOq" ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_me1MgUhJhrDfkcDFCt4WgNRgucI9kqOq') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
