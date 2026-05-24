7249a182898f63c3bc6615657a44275c76464dc5
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2026-05-24 07:18:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(getenv("OWNER_ID", "7250012103"))
 
 LOG = int(getenv("LOG", True))
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz')
+API_URL = getenv("API_URL", 'https://console.nexgenbots.xyz')
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
 API_KEY = getenv("API_KEY", '30DxNexGenBots431c89')
 
